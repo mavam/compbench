72d24e3e105205ea8835bcac928c3c5723504531
Matthias Vallentin <vallentin@icir.org>
2015-09-26 15:32:09 +0200

    Add new algorithms introduced with bundle 0.9.4

--- benchmark.cpp
+++ benchmark.cpp
@@ -49,8 +49,10 @@ auto main() -> int {
 #endif
   std::string buffer{std::istreambuf_iterator<char>{std::cin},
                       std::istreambuf_iterator<char>{}};
-  std::vector<unsigned> libs{RAW, SHOCO, LZ4, MINIZ, LZIP, LZMA20, ZPAQ, LZ4HC,
-                             BROTLI9, ZSTD, LZMA25, BSC, BROTLI11};
+  std::vector<unsigned> libs{RAW, SHOCO, LZ4F, MINIZ, LZIP, LZMA20, ZPAQ, LZ4,
+                             BROTLI9, ZSTD, LZMA25, BSC, BROTLI11, SHRINKER,
+                             CSC20};
+
   std::cout << "algorithm\traw\tpacked\tunpacked\tcompression\tdecompression\n";
   for (auto& use : libs) {
     auto pack_start = std::chrono::high_resolution_clock::now();
